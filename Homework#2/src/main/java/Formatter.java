f737bb93e463f3026179c276464bd4ccb31e72be
Maxim Kalashnik <kalashnikmaxim@mail.ru>
2018-10-27 11:31:35 +0600

    Add main interfaces

---
+++ Homework#2/src/main/java/Formatter.java
@@ -0,0 +1,14 @@
+public class Formatter {
+    private StringBuilder output;
+    private char[] buff;
+
+    public Formatter() {
+        this.output = new StringBuilder();
+        this.buff = new char[2];
+    }
+
+    public String format(String input) {
+        char[] in = input.toCharArray();
+        for ()
+    }
+}
